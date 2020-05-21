1dc731e8a636872e91dd4c87751f6378981fe890
Crowdbotics <team@crowdbotics.com>
2020-05-21 16:19:17 +0000

    Update admin title

--- backend/lizzmay19_may21_mob_17235/urls.py
+++ backend/lizzmay19_may21_mob_17235/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "lizzmay19 may21 mobile1"
-admin.site.site_title = "lizzmay19 may21 mobile1 Admin Portal"
-admin.site.index_title = "lizzmay19 may21 mobile1 Admin"
+admin.site.site_header = "lizzmay19 may21 mobile1rn"
+admin.site.site_title = "lizzmay19 may21 mobile1rn Admin Portal"
+admin.site.index_title = "lizzmay19 may21 mobile1rn Admin"
 
 # swagger
 schema_view = get_schema_view(
